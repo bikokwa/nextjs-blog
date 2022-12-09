15d798a7ed21c3d2b442c53b106419b164ee84f3
bikokwa <34458781+bikokwa@users.noreply.github.com>
2022-12-09 03:58:50 +0300

    updating my description

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,11 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hi, I am Bikokwa. I am a software engineer.</p>
+        <p>
+          Hi, I am Bikokwa. I am a software engineer. I have knowledge in HTML,
+          CSS, and JavaScript. I am current open for a Junior Front End
+          Developer role.
+        </p>
       </section>
     </Layout>
   );
