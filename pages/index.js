4abd73d1f078f7db8041161be3be7f9891c2e58a
bikokwa <34458781+bikokwa@users.noreply.github.com>
2022-12-09 03:44:30 +0300

    Nextjs basics initial

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import Head from "next/head";
+import Layout, { siteTitle } from "../components/layout";
+import utilStyles from "../styles/utils.module.css";
+
+export default function Home() {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>Hi, I am Bikokwa. I am a software engineer.</p>
+      </section>
+    </Layout>
+  );
+}
